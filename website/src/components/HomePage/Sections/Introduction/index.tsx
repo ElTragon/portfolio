07e1bb3cd1b417cb442ee9f60240eccad5d1774f
Mario Lopez <mariodolt@marios-mbp.lan>
2023-03-21 23:49:12 -0700

    better style, needs some text effects. Set you prettier and lint next

--- website/src/components/HomePage/Sections/Introduction/index.tsx
+++ website/src/components/HomePage/Sections/Introduction/index.tsx
@@ -2,14 +2,17 @@ import css from "./index.module.css";
 
 const Introduction = (): JSX.Element => {
   return (
-    <div className={css.mainContainer}>
+    <div className={css.container}>
       <p>It&apos;s a me,</p>
-      <h1>Mario</h1>
+      <h1>
+        Mario
+        <span className={css.fadeIn}> Lopez</span>
+      </h1>
       <h2>Full Stack Engineer</h2>
 
       <h3>
-        I&apos;m a full stack engineer specializing in building (and
-        occasionally designing) exceptional digital experiences.
+        I&apos;m a engineer specializing in building (and occasionally
+        designing) exceptional digital experiences.
       </h3>
     </div>
   );
