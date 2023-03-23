341b843e4673bf820b6c4c7430fb4dbfaa17df90
Mario Lopez <mariodolt@marios-mbp.lan>
2023-03-23 00:20:05 -0700

    some progress

---
+++ src/components/HomePage/Sections/AboutMe/index.tsx
@@ -0,0 +1,43 @@
+import SectionHeader from "../SectionHeader";
+import css from "./index.module.css";
+
+const AboutMe = (): JSX.Element => {
+  return (
+    <div className={css.container}>
+      <SectionHeader num={"01"} title={"About Me"} />
+
+      <p>
+        Hello! I&apos;m Mario and I focus on crafting high-quality digital
+        experiences that are both functional and visually appealing. My career
+        began at HelloAdvisr - turns out you can learn the fundamentals of Web
+        development over a weekend before a interview Monday morning.
+      </p>
+
+      <p>
+        Fast-forward to today, I have had the honor to work at Fintech,
+        Pharmaceutical, and Database companies. Now I days I am looking for the
+        next step in my career to build responsive and reliable products.
+        whenever I can.
+      </p>
+
+      <p>
+        Heres a short list of a few technologies I been working with recently
+      </p>
+
+        <div className={css.techContainer}>
+            <ul>
+                <li>Javascript</li>
+                <li>React</li>
+                <li>Python</li>
+            </ul>
+            <ul>
+                <li>Typescript</li>
+                <li>NextJS</li>
+                <li>Golang</li>
+            </ul>
+        </div>
+    </div>
+  );
+};
+
+export default AboutMe;
--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -1,12 +1,14 @@
 import Page from "../utils/Page";
 import css from "./index.module.css";
-import Introduction from "./Sections/Introduction";
+import Introduction from "./Introduction";
+import AboutMe from "./Sections/AboutMe";
 
 const HomePage = (): JSX.Element => {
   return (
     <Page>
       <main className={css.mainContainer}>
         <Introduction />
+        <AboutMe />
       </main>
     </Page>
   );
